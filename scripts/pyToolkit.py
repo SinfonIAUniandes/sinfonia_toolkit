e288dc949db2ca36b3fddd3f5de64c7e7286b80c
David Cuevas <98664178+dcuevasa@users.noreply.github.com>
2023-06-13 14:50:34 -0500

    Via autonoma service

--- scripts/pyToolkit.py
+++ scripts/pyToolkit.py
@@ -9,24 +9,30 @@ import sys
 from robot_toolkit_msgs.srv import  show_image_srv
 from std_srvs.srv import Empty
 
-
 class PyToolkit:
 
     def __init__(self, session):
-        self.show_image_srv = rospy.Service('tablet/show_image', show_image_srv, self.show_image_srv)
-        self.show_webpage_srv = rospy.Service('tablet/show_webpage', show_image_srv, self.show_webpage_srv)
-        self.show_video_srv = rospy.Service('tablet/show_video', show_image_srv, self.show_video_srv)
-
-        self.ts = session.service("ALTabletService")
+        self.show_srv = rospy.Service('tablet/show_image', show_image_srv, 
+                                      self.show_image_srv)
+        self.hide_tablet_srv = rospy.Service('tablet/hide_tablet', show_image_srv, 
+                                             self.hide_tablet_srv)
+        self.show_webpage_srv = rospy.Service('tablet/show_webpage', show_image_srv, 
+                                              self.show_webpage_srv)
+        self.show_video_srv = rospy.Service('tablet/show_video', show_image_srv, 
+                                            self.show_video_srv)
         self.autonomous_life_service = session.service("ALAutonomousLife")
+        self.disable_autonomous_life_srv = rospy.Service('autonomous_life/disable', Empty, 
+                                                         self.disable_autonomous_life_srv)
+        self.ts = session.service("ALTabletService")
         self.posture_service = session.service("ALRobotPosture")
-
-        self.disable_autonomous_life_srv = rospy.Service('autonomous_life/disable', Empty, self.disable_autonomous_life_srv)
-        
-
     def show_image_srv(self,req):
         url = req.url
         self.ts.showImage(url)
+        time.sleep(5)
+        return None
+
+    def hide_tablet_srv(self,req):
+        self.ts.hide()
         time.sleep(5)
         return None
 
@@ -42,14 +48,14 @@ class PyToolkit:
         time.sleep(5)
         return None
     
-    def stand(self):
-        self.posture_service.goToPosture("Stand", 0.5)
-        print("[INFO]: Robot is in default position")
-    
     def disable_autonomous_life_srv(self,req):
         self.autonomous_life_service.setState("disabled")
         self.stand()
         print("[INFO]: Autonomous life is off")
+    
+    def stand(self):
+        self.posture_service.goToPosture("Stand", 0.5)
+        print("[INFO]: Robot is in default position")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -62,7 +68,8 @@ if __name__ == '__main__':
     try:
         session.connect("tcp://" + args.ip + ":" + str(args.port))
     except RuntimeError:
-        print("Can't connect to Naoqi at ip \"" + args.ip + "\" on port" + str(args.port) + ".\n""Please check your script arguments. Run with -h option for help.")
+        print("Can't connect to Naoqi at ip \"" + args.ip + "\" on port" + str(args.port) + 
+           ".\n""Please check your script arguments. Run with -h option for help.")
         sys.exit(1)
     pytoolkit = PyToolkit(session)
     rospy.init_node('pytoolkit')
